3af97c71a0a405116e2bdce14d1ab4d74b9a5e75
Aizuk <nickbrandariz@gmail.com>
2022-02-08 15:10:38 -0300

    Initial Commit

---
+++ src/main/java/com/alkemy/challengeingreso/challengeingreso/ChallengeIngresoApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.challengeingreso.challengeingreso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeIngresoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeIngresoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alkemy/challengeingreso/challengeingreso/ChallengeIngresoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.challengeingreso.challengeingreso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeIngresoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
