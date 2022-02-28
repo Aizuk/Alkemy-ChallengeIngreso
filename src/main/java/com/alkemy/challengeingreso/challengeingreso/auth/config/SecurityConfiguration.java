d288c61a8afa8c1144065377231d975adbbf774d
Aizuk <nickbrandariz@gmail.com>
2022-02-28 19:45:11 -0300

    se quita el encriptador de password debido a la falta de tiempo para corregir los errores y poder llegar a la entrega

--- src/main/java/com/alkemy/challengeingreso/challengeingreso/auth/config/SecurityConfiguration.java
+++ src/main/java/com/alkemy/challengeingreso/challengeingreso/auth/config/SecurityConfiguration.java
@@ -30,7 +30,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 
     @Override
